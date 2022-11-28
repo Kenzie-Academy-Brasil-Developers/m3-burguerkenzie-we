4ee50def06923344a7e7de46e549284fa9615f3f
wellinton-eugenio <wellington.eliteinfo@gmail.com>
2022-11-27 22:23:56 -0300

    inclued toast events

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { useState, useEffect } from "react";
 import { api } from "./services/api";
 import Global from "./styles/global";
 import { Container } from "./styles/main";
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-const [products, setProducts] = useState([]);
-const [filteredProducts, setFilteredProducts] = useState([]);
-const [currentSale, setCurrentSale] = useState([])
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentSale, setCurrentSale] = useState([])
 
-useEffect(()=>{
+  useEffect(()=>{
   async function getProducts(){
     try {
       const response = await api.get('products')
@@ -22,34 +24,37 @@ useEffect(()=>{
   }
 
   getProducts()
-}, [])
+  }, [])
 
-function handleClick(productid, product){
-  const currentProduct = currentSale.find((prod => prod.id === productid))
-  if(currentProduct === undefined){
-    setCurrentSale([...currentSale, product])
+  function handleClick(productid, product){
+    const currentProduct = currentSale.find((prod => prod.id === productid))
+    if(currentProduct === undefined){
+      toast.success('produto adicionado!')
+      setCurrentSale([...currentSale, product])
+      someCart()
+      
+    } else {
+      toast.error('Produto já adicionado!')
+    } 
+
+  }
+
+  function removeProduct(productid){
+    const newProducs = currentSale.filter((product)=> product.id !== productid)
+    setCurrentSale(newProducs)
+    toast.success('Produto removido!')
     someCart()
-  } else {
-    console.log('product exist')
-  } 
+  }
 
-}
+  function someCart(currentSale){
+    let some = currentSale.reduce((count, product)=>{
+      count += parseFloat(product.price)
+      return(count)
+    }, 0)
+    return some.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+  }
 
-function removeProduct(productid){
-  const newProducs = currentSale.filter((product)=> product.id !== productid)
-  setCurrentSale(newProducs)
-  someCart()
-}
-
-function someCart(currentSale){
-  let some = currentSale.reduce((count, product)=>{
-    count += parseFloat(product.price)
-    return(count)
-  }, 0)
-  return some.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
-}
-
-return (
+  return (
     <div>
       <Global />
       <HeaderApp />
@@ -63,6 +68,20 @@ return (
           someCart={someCart}
           setCurrentSale={setCurrentSale}/>
       </Container>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        />
+        {/* Same as */}
+      <ToastContainer />
       
     </div>
   );
